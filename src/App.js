2e35f72e61a8b3825db4fc9c3f31f236c16ed69d
Max Fehmerling <fehmer@tellonym.me>
2018-10-31 16:43:44 +0100

    added removal of users

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import React from "react";
 import { connect } from "react-redux";
 import "./App.css";
 import NewUserForm from "./User";
+import { remove as removeUser } from "./redux/user/actions";
 
-const App = ({ users }) => (
+const App = ({ users, dispatch }) => (
   <div className="App">
     <header className="App-header">
-      <ul>
-        {users && users.length ? (
-          users.map((user, index) => (
-            <li key={`user-${user.id}`}>{user.name}</li>
-          ))
-        ) : (
-          <li>No Users created</li>
-        )}
-      </ul>
-      <hr />
+      {users && users.length ? (
+        <ul>
+          {users.map((user, index) => (
+            <li key={`user-${user.id}`}>
+              {user.name}{" "}
+              <button onClick={() => dispatch(removeUser({ id: user.id }))}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span>No Users created</span>
+      )}
       <NewUserForm />
     </header>
   </div>
 );
 
-const mapStateToProps = ({ user }) => {
-  return { users: user.users };
-};
+const mapStateToProps = ({ user }) => ({ users: user.users });
 
 export default connect(mapStateToProps)(App);
